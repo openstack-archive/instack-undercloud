b3350a10dec90c726cfc5d5178308c423c3d3dc9
James Slagle <jslagle@redhat.com>
2015-01-20 08:21:17 -0500

    Add needed kernel modules to the ramdisk for hardware-detect

--- elements/ironic-discoverd-ramdisk-instack/dracut-modules/80-ironic-discoverd-ramdisk-instack/module-setup.sh
+++ elements/ironic-discoverd-ramdisk-instack/dracut-modules/80-ironic-discoverd-ramdisk-instack/module-setup.sh
@@ -13,18 +13,17 @@ depends() {
 
 install() {
     set -x
-    # Something in this directory gets missed by the python-deps call below.
     for i in $(find /usr/lib64/python2.7/ -type f); do
         inst $i
     done
     for i in $(find /usr/lib/python2.7/ -type f); do
         inst $i
     done
-    # $moddir/python-deps /bin/hardware-detect | while read dep; do
-        # case "$dep" in
-            # *.so) inst_library $dep ;;
-            # *.py) inst_simple $dep ;;
-            # *) inst $dep ;;
-        # esac
-    # done
+    inst /etc/ssh/sshd_config
+}
+
+installkernel() {
+    instmods sg
+    instmods ipmi_si
+    instmods ipmi_devintf
 }
